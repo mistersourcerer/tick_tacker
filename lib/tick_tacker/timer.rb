ee96a28bb671143e4ee97d7a02a0705e098d33e0
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2011-05-31 00:59:16 -0300

    removing useless code

--- lib/tick_tacker/timer.rb
+++ lib/tick_tacker/timer.rb
@@ -1,46 +1,6 @@
 require 'observer'
-class Fixnum
-  def minutes
-    self * 60
-  end
-
-  def minute
-    self.minutes
-  end
-
-  def seconds
-    self
-  end
-
-  def second
-    self.seconds
-  end
-end
 
 module TickTacker
-  class Ticker
-    def initialize
-      @can_run = true
-    end
-
-    def time_block 
-      start_time = Time.now 
-      Thread.new { yield } 
-      Time.now - start_time 
-    end 
-
-    def repeat_every(seconds) 
-      while @can_run do 
-        time_spent = time_block { yield } # To handle -ve sleep interaval 
-        sleep(seconds - time_spent) if time_spent < seconds 
-      end
-    end
-
-    def stop
-      @can_run = false
-    end
-  end
-
   class Timer
     include Observable
 
